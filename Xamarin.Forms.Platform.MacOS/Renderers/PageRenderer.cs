4922240014c50dc31c042be9cc64ab73a07377ce
Gerald Versluis <gerald@verslu.is>
2020-05-11 13:45:27 +0200

    Some hardcoded color removal

--- Xamarin.Forms.Platform.MacOS/Renderers/PageRenderer.cs
+++ Xamarin.Forms.Platform.MacOS/Renderers/PageRenderer.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.ComponentModel;
 using AppKit;
-using Foundation;
 using Xamarin.Forms.PlatformConfiguration.macOSSpecific;
 
 namespace Xamarin.Forms.Platform.MacOS
 {
+	public class FormsNSView : NSView
+	{
+		readonly IVisualElementRenderer _renderer;
+		public FormsNSView(IVisualElementRenderer renderer)
+		{
+			_renderer = renderer;
+		}
+
+		public override void UpdateLayer()
+		{
+			base.UpdateLayer();
+
+			UpdateBackground();
+		}
+
+		void UpdateBackground()
+		{
+			_renderer.ApplyNativeImageAsync(Page.BackgroundImageSourceProperty, bgImage =>
+			{
+				if (bgImage != null)
+				{
+					Layer.BackgroundColor = NSColor.FromPatternImage(bgImage).CGColor;
+				}
+				else
+				{
+					Color bgColor = _renderer.Element.BackgroundColor;
+
+					Layer.BackgroundColor = bgColor.IsDefault ? ColorExtensions.WindowBackgroundColor.CGColor : bgColor.ToCGColor();
+				}
+			});
+		}
+	}
 	public class PageRenderer : NSViewController, IVisualElementRenderer, IEffectControlProvider
 	{
 		bool _init;
@@ -19,7 +50,7 @@ namespace Xamarin.Forms.Platform.MacOS
 
 		public PageRenderer()
 		{
-			View = new NSView { WantsLayer = true };
+			View = new FormsNSView(this) { WantsLayer = true };
 		}
 
 		void IEffectControlProvider.RegisterEffect(Effect effect)
@@ -93,7 +124,6 @@ namespace Xamarin.Forms.Platform.MacOS
 		public override void ViewDidLayout()
 		{
 			base.ViewDidLayout();
-			UpdateBackground();
 		}
 
 		protected override void Dispose(bool disposing)
@@ -179,19 +209,8 @@ namespace Xamarin.Forms.Platform.MacOS
 
 		void UpdateBackground()
 		{
-			this.ApplyNativeImageAsync(Page.BackgroundImageSourceProperty, bgImage =>
-			{
-				if (bgImage != null)
-				{
-					View.Layer.BackgroundColor = NSColor.FromPatternImage(bgImage).CGColor;
-				}
-				else
-				{
-					Color bgColor = Element.BackgroundColor;
-
-					View.Layer.BackgroundColor = bgColor.IsDefault ? ColorExtensions.WindowBackgroundColor.CGColor : bgColor.ToCGColor();
-				}
-			});
+			// Moved bg logic to NSView inheritance to allow for dynamic updating through OS Theme
+			View.UpdateLayer();
 		}
 
 		void UpdateTitle()
